2e42d56b4e31277c5283f937d8db4dc295c27aaf
INMihaylov19 <INMihaylov19@codingburgas.bg>
2021-10-22 20:36:33 +0300

    Add controls.cpp -Add functions for coordinates and colours

--- maze-arizona/controls.cpp
+++ maze-arizona/controls.cpp
@@ -1,11 +1,28 @@
 #include <conio.h>
 #include <iostream>
+#include <windows.h>
+#include "controls.h"
 using namespace std;
 
 #define KEY_UP    72
 #define KEY_LEFT  75
 #define KEY_RIGHT 77
 #define KEY_DOWN  80
+
+int SetColor[3] = { 13, 7, 7 };
+
+void color(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x; // your X cordinates
+    coord.Y = y; // your Y cordinates
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 void controls()
 {

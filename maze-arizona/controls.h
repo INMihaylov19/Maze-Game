8c094cfd70eb0ed25f35f88a6b6cad49c3a9724a
INMihaylov19 <INMihaylov19@codingburgas.bg>
2021-10-22 20:36:47 +0300

    Add controls.h

---
+++ maze-arizona/controls.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+int SetColor[3] = { 13, 7, 7 };
+
+void color(int color);
+
+void gotoxy(int x, int y);
+
+void controls();
+

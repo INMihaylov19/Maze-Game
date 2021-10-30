97befd6b422f6baa429a5148db399d0e7eca5068
Yoanna Simeonova <YVSimeonova19@codingburgas.bg>
2021-10-30 17:30:57 +0300

    Edit details in FrontEnd.cpp

--- maze-arizona/FrontEnd.cpp
+++ maze-arizona/FrontEnd.cpp
@@ -8,10 +8,10 @@ const char field = char(219);
 void check(int size) // checks where to put a square or empty space
 {
 	//declare the grid for the maze
-	int** grid = new int* [size];
+	char** grid = new char* [size];
 	for (int i = 0; i < size; i++)
 	{
-		grid[i] = new int[size];
+		grid[i] = new char[size];
 	}
 
 	maze_grid(size, grid);
@@ -20,9 +20,13 @@ void check(int size) // checks where to put a square or empty space
 	{
 		for (int j = 0; j < size; j++)
 		{
-			if (grid[i][j] == 2)
+			if (grid[i][j] == 'x')
 			{
 				cout << field << field;
+			}
+			else if (grid[i][j] == 'y')
+			{
+				cout << "  ";
 			}
 		}
 		cout << endl;

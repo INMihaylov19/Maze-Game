38f72b6470e51f519e06a7bdc11cd2c78d16ce5f
Yoanna Simeonova <YVSimeonova19@codingburgas.bg>
2021-10-30 17:10:02 +0300

    Add functions to BackEnd.h

--- maze-arizona/BackEnd.h
+++ maze-arizona/BackEnd.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <stdlib.h>
 #include<time.h>
-#include <string>
+#include <stack>
 
-void random(int* i, int* j);
+void assign_new_values(const stack<pair<int, int>>& stack, int& i, int& j);
+
+void check_path_cell(char** arr, const stack<pair<int, int>>& stack, int& i, int& j);
 
 void maze_grid(int size, int** grid);

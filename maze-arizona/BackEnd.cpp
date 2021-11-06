3880a19439d7618c715c908f62836a965124bcb8
Yoanna Simeonova <YVSimeonova19@codingburgas.bg>
2021-11-06 15:17:41 +0200

    Add extra conditions BackEnd.cpp

--- maze-arizona/BackEnd.cpp
+++ maze-arizona/BackEnd.cpp
@@ -9,22 +9,22 @@ void assign_new_values(pair<int, int> pair, int& i, int& j)
 	j = pair.second;
 }
 
-void check_path_cell(char** arr, pair<int, int> pair, int& i, int& j)
+void check_path_cell(char** arr, int size, pair<int, int> pair, int& i, int& j)
 { //make the cell between the two filled cells a path cell
     if (i == pair.first) //if it's on the same row
     {
-        if (j + 2 == pair.second) //if it's after (right) the current cell
+        if (j < size - 1 && j + 2 == pair.second) //if it's after (right) the current cell
             arr[pair.first][j + 1] = 'y';
 
-        else if (j - 2 == pair.second) //if it's before (left) the current cell
+        else if (j > 1 && j - 2 == pair.second) //if it's before (left) the current cell
             arr[pair.first][j - 1] = 'y';
     }
     else if (j == pair.second) // if it's in the same column
     {
-        if (i + 2 == pair.first) //if it's above the current cell
+        if (i < size -1 && i + 2 == pair.first) //if it's above the current cell
             arr[i + 1][pair.second] = 'y';
 
-        else if (i - 2 == pair.first) //if it's below the current cell
+        else if (i > 1 && i - 2 == pair.first) //if it's below the current cell
             arr[i - 1][pair.second] = 'y';
     }
 }
@@ -34,8 +34,8 @@ void maze_grid(int size, char** arr)
     //cell count
     int counter = 0;
 
-    // stena - x
-    // zapulnena kletka - y
+    // wall - x
+    // path cell - y
     for (int i = 0; i < size; i++)
     {
         arr[0][i] = 'x';
@@ -49,6 +49,7 @@ void maze_grid(int size, char** arr)
     //available spaces around current cell
     pair<int, int> available;
     bool available_arr[4] = { false, false, false, false };
+    bool av_check = false;
 
     //the path used for backtracking
     stack<pair<int, int>> path;
@@ -59,24 +60,28 @@ void maze_grid(int size, char** arr)
     //continues until all cells have been assigned a value
     while (counter != size*size)
     {
-        if (arr[i][j + 2] != 'y' && arr[i][j + 2] != 'x') //East
+        if (j < size - 1 && arr[i][j + 2] != 'y' && arr[i][j + 2] != 'x') //East
         {
             available_arr[0] = true;
+            av_check = true;
         }
-        if (arr[i + 2][j] != 'y' && arr[i + 2][j] != 'x') //South
+        if (i < size - 1 && arr[i + 2][j] != 'y' && arr[i + 2][j] != 'x') //South
         {
             available_arr[1] = true;
+            av_check = true;
         }
         if (j > 1 && arr[i][j - 2] != 'y' && arr[i][j - 2] != 'x') //West
         {
             available_arr[2] = true;
+            av_check = true;
         }
         if (i > 1 && arr[i - 2][j] != 'y' && arr[i - 2][j] != 'x') //North
         {
             available_arr[3] = true;
+            av_check = true;
         }
 
-        if (available_arr) //
+        if (av_check) //
         {
             // Choose one available neighbour at random
             srand((unsigned)time(0));
@@ -98,12 +103,12 @@ void maze_grid(int size, char** arr)
                 available.second = j + 2;
 
                 //make the cell between the two filled cells a path cell
-                check_path_cell(arr, available, i, j);
+                check_path_cell(arr, size, available, i, j);
 
                 //assign the new cell's values to the coordinate variables
                 assign_new_values(available, i, j);
 
-                path.push({ i, j + 2 });
+                path.push(available);
                 counter += 2;
                 break;
 
@@ -114,12 +119,12 @@ void maze_grid(int size, char** arr)
                 available.second = j;
 
                 //make the cell between the two filled cells a path cell
-                check_path_cell(arr, available, i, j);
+                check_path_cell(arr, size, available, i, j);
 
                 //assign the new cell's values to the coordinate variables
                 assign_new_values(available, i, j);
 
-                path.push({ i + 2, j });
+                path.push(available);
                 counter += 2;
                 break;
 
@@ -130,12 +135,12 @@ void maze_grid(int size, char** arr)
                 available.second = j - 2;
 
                 //make the cell between the two filled cells a path cell
-                check_path_cell(arr, available, i, j);
+                check_path_cell(arr, size, available, i, j);
 
                 //assign the new cell's values to the coordinate variables
                 assign_new_values(available, i, j);
 
-                path.push({ i, j - 2 });
+                path.push(available);
                 counter += 2;
                 break;
 
@@ -146,15 +151,17 @@ void maze_grid(int size, char** arr)
                 available.second = j;
 
                 //make the cell between the two filled cells a path cell
-                check_path_cell(arr, available, i, j);
+                check_path_cell(arr, size, available, i, j);
 
                 //assign the new cell's values to the coordinate variables
                 assign_new_values(available, i, j);
 
-                path.push({ i - 2, j });
+                path.push(available);
                 counter += 2;
                 break;
             }
+
+            av_check = false;
         }
         else
         {
--- maze-arizona/BackEnd.h
+++ maze-arizona/BackEnd.h
@@ -6,6 +6,6 @@ using namespace std;
 
 void assign_new_values(pair<int, int> pair, int& i, int& j);
 
-void check_path_cell(char** arr, pair<int, int> pair, int& i, int& j);
+void check_path_cell(char** arr, int size, pair<int, int> pair, int& i, int& j);
 
 void maze_grid(int size, char** grid);
